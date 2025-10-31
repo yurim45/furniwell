004bfc50fa7061606a1fa0a45a3c35e88e101134
yrkim <icho0405@gmail.com>
2025-10-31 17:12:14 +0900

    fix: meta desc 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,12 @@ const inter = Inter({
   display: 'swap',
 });
 
+const DESC =
+  '단순한 거래가 아닌 장기적 파트너로 함께 성장합니다. 인도네시아 현지 직영 공장으로써 가구 및 파렛트 제작 수출, 종합 인테리어 서비스를 제공하는 (주)퍼니웰입니다. We grow together as long-term partners, not just simple transactions. As a directly-operated factory in Indonesia, Furniwell Co., Ltd. manufactures and exports furniture and pallets, as well as providing comprehensive interior design services.';
+
 export const metadata: Metadata = {
   title: '퍼니웰(주) FURNIWELL',
-  description: '원목가구 목재 전문업체 PT. FURNIWELL MAKMUR ABADI',
+  description: DESC,
   icons: {
     icon: '/favicon.ico',
   },
@@ -35,7 +38,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: '/images/og_image.jpg',
     title: '퍼니웰(주) FURNIWELL',
-    description: '원목가구 목재 전문업체 PT. FURNIWELL MAKMUR ABADI',
+    description: DESC,
     siteName: 'FURNIWELL',
     type: 'website',
   },
